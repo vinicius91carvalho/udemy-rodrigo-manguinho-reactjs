3d8869bf6f49d39cefaf0036f40a4d9b53af056a
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-06 10:24:37 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
 module.exports = {
   roots: ['<rootDir>/tests'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
-    '.+\\.ts(x)?$': 'ts-jest'
+    '.+\\.ts(x?)$': 'ts-jest'
   },
   moduleNameMapper: {
     '@/tests/(.*)': '<rootDir>/tests/$1',
-    '@/(.*)': '<rootDir>/src/$1'
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
   }
 }
