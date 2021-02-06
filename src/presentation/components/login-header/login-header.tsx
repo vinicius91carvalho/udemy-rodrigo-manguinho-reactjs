9b8a2d346d2c2b6407ffe6d6e7dd0cc384923f91
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-06 12:48:37 -0300

    chore: add editor config

--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -5,8 +5,8 @@ import React, { memo } from 'react'
 const LoginHeaderComponent: React.FC = () => {
   return (
     <header className={Styles.header}>
-        <Logo />
-        <h1>4Dev - Enquetes para Programadores</h1>
+      <Logo />
+      <h1>4Dev - Enquetes para Programadores</h1>
     </header>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -4,17 +4,17 @@ import React from 'react'
 
 export const Login: React.FC = () => {
   return (
-        <div className={Styles.login}>
-            <LoginHeader />
-            <form className={Styles.form}>
-                <h2>Login</h2>
-                <Input type="email" name="email" placeholder="Digite seu e-mail" />
-                <Input type="password" name="password" placeholder="Digite sua senha"/>
-                <button className={Styles.submit} type="submit">Entrar</button>
-                <span className={Styles.link}>Criar conta</span>
-                <FormStatus />
-            </form>
-            <Footer />
-        </div>
+    <div className={Styles.login}>
+      <LoginHeader />
+      <form className={Styles.form}>
+          <h2>Login</h2>
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input type="password" name="password" placeholder="Digite sua senha"/>
+          <button className={Styles.submit} type="submit">Entrar</button>
+          <span className={Styles.link}>Criar conta</span>
+          <FormStatus />
+      </form>
+      <Footer />
+    </div>
   )
 }
