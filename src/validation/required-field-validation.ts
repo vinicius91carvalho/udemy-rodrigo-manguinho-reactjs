f9afbdc31274bdea6baf0012f251895c79b83f03
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-10 15:19:27 -0300

    feat: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field-validation.ts
+++ src/validation/required-field-validation.ts
@@ -7,6 +7,6 @@ export class RequiredFieldValidation implements FieldValidation {
   }
 
   validate ({ value }: FieldValidation.Params): FieldValidation.Result {
-    return new RequiredFieldError()
+    return value ? null : new RequiredFieldError()
   }
 }
