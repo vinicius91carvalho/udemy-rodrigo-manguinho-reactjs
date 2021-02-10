ee81d09e2ecbc4fae8421db1a73c4456fbd58461
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-10 13:07:03 -0300

    test: ensure Login present error and hide spinner on authentication failure

--- tests/domain/mocks/mock-authentication.ts
+++ tests/domain/mocks/mock-authentication.ts
@@ -2,13 +2,13 @@ import { Authentication } from '@/domain/usecases/authentication'
 import { mockAccountModel } from '@/tests/domain/mocks'
 
 export class AuthenticationSpy implements Authentication {
-  account = mockAccountModel()
+  result = mockAccountModel()
   params: Authentication.Params
   callsCount = 0
 
   async auth (params: Authentication.Params): Promise<Authentication.Result> {
     this.params = params
     this.callsCount++
-    return this.account
+    return this.result
   }
 }
--- tests/presentation/pages/login.spec.tsx
+++ tests/presentation/pages/login.spec.tsx
@@ -1,7 +1,8 @@
+import { InvalidCredentialsError } from '@/domain/errors'
 import { AuthenticationSpy } from '@/tests/domain/mocks'
 import { ValidationStub } from '@/tests/presentation/mocks'
 import { Login } from '@/presentation/pages/login/login'
-import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
+import { render, RenderResult, fireEvent, cleanup, waitFor } from '@testing-library/react'
 import faker from 'faker'
 import React from 'react'
 
@@ -127,5 +128,17 @@ describe('Login Component', () => {
     populateEmailField(sut)
     fireEvent.submit(sut.getByTestId('form'))
     expect(authenticationSpy.callsCount).toBe(0)
+  })
+
+  test('Should present error if Authentication fails', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    const error = new InvalidCredentialsError()
+    jest.spyOn(authenticationSpy, 'auth').mockReturnValueOnce(Promise.reject(error))
+    simulateValidSubmit(sut)
+    const errorWrap = sut.getByTestId('error-wrap')
+    await waitFor(() => errorWrap)
+    const mainError = sut.getByTestId('main-error')
+    expect(mainError.textContent).toBe(error.message)
+    expect(errorWrap.childElementCount).toBe(1)
   })
 })
