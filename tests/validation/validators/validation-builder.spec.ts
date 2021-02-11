7a1e14efacec91022269e2bdbb4466e67e13466a
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-11 14:49:57 -0300

    test: ensure ValidationBuilder has a required helper

---
+++ tests/validation/validators/validation-builder.spec.ts
@@ -0,0 +1,8 @@
+import { ValidationBuilder, RequiredFieldValidation } from '@/validation/validators'
+
+describe('ValidationBuilder', () => {
+  test('Should return RequiredFieldValidation', () => {
+    const validations = ValidationBuilder.field('any_field').required().build()
+    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+})
