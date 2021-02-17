d5fc95ca5938f6eb93774e0ad8753a629d9abc1f
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-02-17 20:14:29 -0300

    feat: make a factory to save access token for Login component

--- src/main/factories/pages/login-factory.tsx
+++ src/main/factories/pages/login-factory.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { makeRemoteAuthentication } from '@/main/factories/usecases/remote-authentication-factory'
+import { makeLocalSaveAccessToken } from '@/main/factories/usecases/local-save-access-token-factory'
 import { Login } from '@/presentation/pages/login/login'
 import { makeLoginValidations } from './login-validation-factory'
 
@@ -8,7 +9,7 @@ export const makeLogin: React.FC = () => {
         <Login
             authentication={makeRemoteAuthentication('/login')}
             validation={makeLoginValidations()}
-            saveAccessToken={null}
+            saveAccessToken={makeLocalSaveAccessToken()}
         />
   )
 }
