a1fe12ac9b90d3d9af6f2c499d4c1db6a82018d2
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-03-14 15:22:02 -0300

    fix: message of email in use error

--- src/domain/errors/email-in-use-error.ts
+++ src/domain/errors/email-in-use-error.ts
@@ -1,6 +1,6 @@
 export class EmailInUseError extends Error {
   constructor () {
-    super('Eesse e-mail já está em uso')
+    super('Esse e-mail já está em uso')
     this.name = 'EmailInUseError'
   }
 }
